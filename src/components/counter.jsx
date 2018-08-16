b5f53464b14dcc46abf1b19aed29c5d70037fae4
Coco Guevara <coco.guevara@gmail.com>
2018-08-16 14:30:40 -0500

    Mungoal

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,13 +6,14 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>increment</button>
       </React.Fragment>
     );
   }
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
