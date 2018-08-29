785f92105c62462539f0acb51fd0d8d17638ad5b
Coco Guevara <coco.guevara@gmail.com>
2018-08-29 14:44:57 -0500

    Lifting the state to share data with children components and NavBar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
+  /*   state = {
     value: this.props.counter.value
-  };
+  }; */
 
-  handleIncrement = () => {
+  /*   handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
-  };
+  }; */
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -30,11 +30,11 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
